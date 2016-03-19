a06af0a691286eaa0ce671e876b043f160bf9e56
lou <email": null>
2016-03-20 12:39:54 +1300

    Add express

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors());
 
 app.use('/', routes);
 app.use('/v1/messages', users);
